e01aa0af6bfca4e863fb7baab1e49986be47b6c7
Denismaka <makadenis370@gmail.com>
2025-03-26 19:30:08 +0100

    Debut du projet

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Innovatech</title>
+  <link rel="stylesheet" href="/assets/bootstrap-icons/bootstrap-icons.css">
+  <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/style.css" />
+  <link rel="stylesheet" href="/assets/font/fontawesome/css/all.min.css">
+</head>
+
+<body>
+
+
+  <script src="/assets/Js/app.js"></script>
+  <script src="/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="/assets/scrollreveal/dist/scrollreveal.min.js"></script>
+</body>
+
+</html>
