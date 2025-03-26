23aad1df478c8c6658fb517eb1a10d26df2ad68e
Denismaka <makadenis370@gmail.com>
2025-03-26 19:36:34 +0100

    Mise en place de la structure Html du Header

--- index.php
+++ index.php
@@ -17,6 +17,27 @@
     <i class="fas fa-arrow-up icone"></i>
   </div>
 
+  <!-- ############## -->
+  <!-- Header -->
+  <!-- ############## -->
+  <header>
+    <a href="index.php" class="logo">
+      <img src="/assets/images/website_logo.png" class="img-fluid rounded">
+      Innovatech
+    </a>
+    <div class="menu-toggle" id="mobile-menu">
+      <i class="fas fa-bars"></i>
+    </div>
+    <ul class="nav-list" id="nav-list">
+      <li><a href="/index.php#home" class="text-capitalize">home</a></li>
+      <li><a href="/index.php#about" class="text-capitalize">about</a></li>
+      <li><a href="/index.php#services" class="text-capitalize">services</a></li>
+      <li><a href="/index.php#section" class="text-capitalize">blog</a></li>
+      <li><a href="/index.php#contact" class="text-capitalize">contact</a></li>
+    </ul>
+  </header>
+
+
 
   <script src="/assets/Js/app.js"></script>
   <script src="/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
